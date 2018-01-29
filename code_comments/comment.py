5429207c453e8cc4b27efebd5028f4e7f168f2ec
baningbo <baningbo@example.com>
2018-01-29 19:10:03 +0800

    fixed problem of display of comments in mysql, check https://github.com/Automattic/trac-code-comments-plugin/pull/26/files

--- code_comments/comment.py
+++ code_comments/comment.py
@@ -159,6 +159,8 @@ class CommentJSONEncoder(json.JSONEncoder):
             for_json = dict([(name, getattr(o, name)) for name in o.__dict__ if isinstance(getattr(o, name), (basestring, int, list, dict))])
             for_json['formatted_date'] = o.formatted_date()
             for_json['permalink'] = o.href()
+            for_json['id'] = o.id
+            for_json['line'] = o.line
             return for_json
         else:
             return json.JSONEncoder.default(self, o)
